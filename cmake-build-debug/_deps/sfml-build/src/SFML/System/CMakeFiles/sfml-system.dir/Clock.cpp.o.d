bb20e57f8298dcf8457ab11ab30f7cd04f239aee
Max <53878520+maxprograms@users.noreply.github.com>
2025-11-08 14:37:24 -0500

    Delete cmake-build-debug directory

--- cmake-build-debug/_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Clock.cpp.o.d
+++
@@ -1,8 +0,0 @@
-_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Clock.cpp.o: \
-  /Users/kaylee.driscoll/Desktop/Geofiltered-Hospital-Rankings/cmake-build-debug/_deps/sfml-src/src/SFML/System/Clock.cpp \
-  /Users/kaylee.driscoll/Desktop/Geofiltered-Hospital-Rankings/cmake-build-debug/_deps/sfml-src/include/SFML/System/Clock.hpp \
-  /Users/kaylee.driscoll/Desktop/Geofiltered-Hospital-Rankings/cmake-build-debug/_deps/sfml-src/include/SFML/System/Export.hpp \
-  /Users/kaylee.driscoll/Desktop/Geofiltered-Hospital-Rankings/cmake-build-debug/_deps/sfml-src/include/SFML/Config.hpp \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX15.5.sdk/usr/include/TargetConditionals.h \
-  /Users/kaylee.driscoll/Desktop/Geofiltered-Hospital-Rankings/cmake-build-debug/_deps/sfml-src/include/SFML/System/Time.hpp \
-  /Users/kaylee.driscoll/Desktop/Geofiltered-Hospital-Rankings/cmake-build-debug/_deps/sfml-src/src/SFML/System/Unix/ClockImpl.hpp
--- cmake-build-debug/_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Time.cpp.o.d
+++
@@ -1,6 +0,0 @@
-_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Time.cpp.o: \
-  /Users/kaylee.driscoll/Desktop/Geofiltered-Hospital-Rankings/cmake-build-debug/_deps/sfml-src/src/SFML/System/Time.cpp \
-  /Users/kaylee.driscoll/Desktop/Geofiltered-Hospital-Rankings/cmake-build-debug/_deps/sfml-src/include/SFML/System/Time.hpp \
-  /Users/kaylee.driscoll/Desktop/Geofiltered-Hospital-Rankings/cmake-build-debug/_deps/sfml-src/include/SFML/System/Export.hpp \
-  /Users/kaylee.driscoll/Desktop/Geofiltered-Hospital-Rankings/cmake-build-debug/_deps/sfml-src/include/SFML/Config.hpp \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX15.5.sdk/usr/include/TargetConditionals.h
